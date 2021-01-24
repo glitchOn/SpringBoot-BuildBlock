b77fe4cb96fa232e951887a25c4ce9aac10becd0
glitchOn <glitchOn>
2021-01-25 02:57:23 +0530

    First Commit

---
+++ src/main/java/com/ck/restservices/SpringbootBuildblockApplication.java
@@ -0,0 +1,13 @@
+package com.ck.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildblockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildblockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ck/restservices/SpringbootBuildblockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ck.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBuildblockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
